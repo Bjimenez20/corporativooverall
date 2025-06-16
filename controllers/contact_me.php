55a2e316b7b097449c6a18eb70e82c662e7c9318
Bjimenez20 <bjimenez@overall.com.co>
2025-06-16 19:25:18 +0000

    chore: modificacion de unidades

--- controllers/contact_me.php
+++ controllers/contact_me.php
@@ -1,6 +1,5 @@
 <?php
 include_once('./../connection/index.php');
-require_once('./mail/PHPMailer.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
@@ -33,166 +32,60 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         switch ($unidad) {
             case 'Executive Search':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
-                break;
-
-            case 'Talent Expat':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Executive_search.php');
                 break;
 
             case 'Talent Solutions':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Talent_solutions.php');
                 break;
 
             case 'Permanent Placement':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Permanent_placement.php');
                 break;
 
             case 'Marketing':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Marketing.php');
                 break;
 
             case 'Trade Marketing':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Trade_marketing.php');
                 break;
 
             case 'Marketing Digital':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Marketing_digital.php');
                 break;
 
             case 'Outsourcing Staffing':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Outsourcing_staffing.php');
                 break;
 
             case 'Facility Managment':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Facility_management.php');
                 break;
 
             case 'Servicios Temporales':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                 require('email/Servicios_temporales.php');
                 break;
 
             case 'Payroll':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Payroll.php');
                 break;
 
             case 'Contact Center':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Contact_center.php');
                 break;
 
             case 'Gestion Logistica':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                 require('email/logistica.php');
                 break;
 
             case 'Servicios Industriales':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                 require('email/Servicios_industriales.php');
                 break;
 
             case 'Servicios Estrategicos Salud':
-                $subject = $unidad;
-                $mail->addAddress('bjimenez@overall.com.co');
+                require('email/Sector_salud.php');
                 break;
         }
-
-        // $subject = "NUEVA SOLICITUD DE " . $unidad . " DESDE CORPORATIVO OVERALL";
-        // $mail->addAddress('bjimenez@overall.com.co');
-
-        $mail->Subject = $subject;
-        $mail->Body = '
-        <!DOCTYPE html
-            PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:box-shadow="http://www.w3.org/1999/xhtml">
-        
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </head>
-        
-        <body style="background-color:#eee;">
-            <div
-                style="background-color:#eee;margin:0;padding:0;font-family:Roboto,Arial,sans-serif;font-weight:500;color:#F0F0F0;font-size:16px;">
-                <p>&nbsp;</p>
-                <table style="width: 100%; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
-                    <tbody>
-                        <tr style="font-size: 14px; color:#545f75">
-                            <td>&nbsp;</td>
-                            <td style="width: 600px;">
-                                <table style="width: 100%; border-collapse: collapse;background-color: #fff;" border="0"
-                                    cellspacing="0" cellpadding="0"> 
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div style="padding: 30px 0; text-align: center;background-color:#1e5fa4;">
-                                                    <div><img src="https://i.imgur.com/IA6URai.png" alt="SVGator"
-                                                            width="140" height="35" /></div>
-                                                </div>
-                                                <div style="line-height: 150%;">
-                                                    <div style="padding: 60px 50px;">
-                                                        <p style="margin: 20px 0; font-weight: 700;">¡Tienes un nuevo mensaje!</p>
-                                                        <p style="margin: 20px 0;">Los datos del contacto son:</p>
-                                                        <ul>
-                                                        <li><b>Nombre completo: </b>' . $full_name . '</li>
-                                                        <li><b>Cargo: </b>' . $position . '</li>
-                                                        <li><b>Empresa: </b>' . $company . '</li>
-                                                        <li><b>Sector empresarial: </b>' . $business_sector . '</li>
-                                                        <li><b>Correo: </b>' . $email . '</li>
-                                                        <li><b>Pais: </b>' . $country . '</li>
-                                                        <li><b>Unidad: </b>' . $unidad . '</li>
-                                                        <li><b>Mensaje: </b>' . $message . '</li>
-                                                        </ul>
-        
-                                                        <p style="margin: 20px 0 0;">Recuerda que esto es un correo automático, por favor no responder.</p>
-        
-                                                        <div style="margin: 45px 0; text-align:left;">
-                                                            <a style="display: inline-block;text-decoration: none; background-color: #004e96; border: none; border-radius: 4px; color: #ffffff; font-size: 16px; line-height: 1; outline: none; text-align: center; padding: 13px 30px; font-weight: bold;"
-                                                                href="https://corporativooverall.com/">Ir al portal</a>
-                                                        </div>
-        
-                                                        <div style="margin: 0;">
-                                                            <p style="margin:0; line-height: 150%;">Gracias por hacer parte de
-                                                                nuestro equipo,<br /> Corporativo Overall</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                            <td>&nbsp;</td>
-                        </tr>
-                        <tr style="color: white; text-align: center;">
-                            <td>&nbsp;</td>
-                            <td style="">
-                                <div style="color: #172032; line-height: 1.5; margin: 25px 0; font-size: 14px;">&copy; 2025
-                                    Corporativo Overall. All rights reserved.<br /></div>
-                            </td>
-                            <td>&nbsp;</td>
-                        </tr>
-        
-                    </tbody>
-                </table>
-            </div>
-        </body>
-        
-        </html>';
-
-        $mail->send();
-    } else {
-        echo "Error: " . mysqli_error($connection);
     }
 }
---
+++ controllers/email/Permanent_placement.php
@@ -0,0 +1,111 @@
+<?php
+include_once('../../connection/index.php');
+require_once('../mail/PHPMailer.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $full_name = $_POST['full_name'];
+    $position = $_POST['position'];
+    $company = $_POST['company'];
+    $business_sector = $_POST['business_sector'];
+    $email = $_POST['email'];
+    $country = $_POST['country'];
+    $unidad = $_POST['unidad'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO `contacts` (`full_name`, `position`, `company`, `business_sector`, `email`, `country`, `unidad`, `message`) VALUES ('$full_name', '$position', '$company', '$business_sector', '$email', '$country', '$unidad', '$message')";
+
+    $query = mysqli_query($connection, $sql);
+
+    if ($query) {
+        $sqlSelect = mysqli_query($connection, "SELECT * FROM contacts ORDER BY id DESC LIMIT 1");
+
+        while ($date = mysqli_fetch_array($sqlSelect)) {
+            $full_name = $date['full_name'];
+            $position = $date['position'];
+            $company = $date['company'];
+            $business_sector = $date['business_sector'];
+            $email = $date['email'];
+            $country = $date['country'];
+            $unidad = $date['unidad'];
+            $message = $date['message'];
+        }
+
+        $subject = "Requerimiento de contratación directa";
+        $mail->addAddress('bjimenez@overall.com.co');
+
+        $mail->Subject = $subject;
+        $mail->Body = '
+       <!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:box-shadow="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body style="background-color:#eee;">
+    <div
+        style="background-color:#eee;margin:0;padding:0;font-family:Roboto,Arial,sans-serif;font-weight:500;color:#F0F0F0;font-size:16px;">
+        <p>&nbsp;</p>
+        <table style="width: 100%; border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+                <tr style="font-size: 14px; color:#545f75">
+                    <td>&nbsp;</td>
+                    <td style="width: 600px;">
+                        <table style="width: 100%; border-collapse: collapse;background-color: #fff;" border="0"
+                            cellspacing="0" cellpadding="0">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div style="padding: 30px 0; text-align: center;background-color:#1e5fa4;">
+                                            <div><img src="https://i.imgur.com/IA6URai.png" alt="SVGator" width="140"
+                                                    height="35" /></div>
+                                        </div>
+                                        <div style="line-height: 150%;">
+                                            <div style="padding: 60px 50px;">
+                                                <p style="margin: 20px 0;">Buen día,</p>
+                                                <p style="margin: 20px 0;">
+                                                    Estoy interesado(a) en conocer su servicio de contratación directa para perfiles clave. Me gustaría agendar una asesoría para entender mejor el alcance del servicio. 
+                                                </p>
+                                                <p style="margin: 20px 0;">
+                                                    Gracias por su tiempo.
+                                                </p>
+                                                <p style="margin: 20px 0;">
+                                                    Atentamente,
+                                                </p>
+                                                <ul>
+                                                    <li><b>Nombre completo: </b>' . $full_name . '</li>
+                                                    <li><b>Empresa: </b>' . $company . '</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr style="color: white; text-align: center;">
+                    <td>&nbsp;</td>
+                    <td style="">
+                        <div style="color: #172032; line-height: 1.5; margin: 25px 0; font-size: 14px;">&copy; 2025
+                            Corporativo Overall. All rights reserved.<br /></div>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>';
+
+        $mail->send();
+    } else {
+        echo "Error: " . mysqli_error($connection);
+    }
+}
